41fcd8829c31716960c21931fe118919aa2e619d
Hamza <153912992+MuhammadHamzaAslam@users.noreply.github.com>
2024-06-12 15:52:09 +0500

    done

--- app.js
+++ app.js
@@ -1,42 +1,3 @@
-// const signUpButton = document.getElementById('signUp');
-// const signInButton = document.getElementById('signIn');
-// const container = document.getElementById('container');
-
-// signUpButton.addEventListener('click', () => {
-// 	container.classList.add("right-panel-active");
-// });
-
-// signInButton.addEventListener('click', () => {
-// 	container.classList.remove("right-panel-active");
-// });
-
-
-// let signUpBtn = document.querySelector('.btn')
-
-// signUpBtn.addEventListener('click',()=>{
-//     let createName = document.getElementById('createName').value
-//     let createEmail = document.getElementById('createEmail').value
-//     let createPass = document.getElementById('createPass').value
-    
-//     localStorage.setItem('mail',createEmail)
-//     localStorage.setItem('name',createName)
-//     localStorage.setItem('pass',createPass)
-//     localStorage.setItem('tr',true)
-//     window.location.href = 'index.html'
-// })
-
-// let loginBtn = document.querySelector('.loginBtn')
-// loginBtn.addEventListener('click',()=>{
-//     let mail = document.getElementById('email').value
-//     let pass = document.getElementById('pass').value
-//     if (mail == localStorage.getItem('mail') && pass == localStorage.getItem('pass')){
-//         window.location.href = 'main.html'
-//         alert('You Logged')
-//     }
-//     else{
-//         alert('No Logged')
-//     }
-// })
 
 
 const signUpButton = document.getElementById('signUp');
@@ -50,6 +11,7 @@ signUpButton.addEventListener('click', () => {
 signInButton.addEventListener('click', () => {
     container.classList.remove("right-panel-active");
 });
+
 
 let signUpBtn = document.querySelector('.btn');
 
@@ -73,10 +35,19 @@ loginBtn.addEventListener('click', (event) => {
     let mail = document.getElementById('email').value;
     let pass = document.getElementById('pass').value;
     if (mail === localStorage.getItem('mail') && pass === localStorage.getItem('pass')) {
-        localStorage.setItem('tr', 'true'); // Set login status to true
+        localStorage.setItem('tr', 'true'); 
         window.location.href = 'main.html';
+        mail.value = ''
+        pass.value = ''
     } else {
-        alert('No Logged');
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Email or Password is wrong",
+            footer: '<a href="#">Why do I have this issue?</a>'
+          });
+          mail.value = ''
+          pass.value = ''
     }
 });
 
